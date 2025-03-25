56aafcbfb27190b6d2f178bc8dc3cc453c1fc0ff
MalcolnLMR <malcolnlmr@gmail.com>
2025-03-25 09:01:09 -0300

    remove unused directives

--- scrap.py
+++ scrap.py
@@ -1,4 +1,4 @@
-import os, sys, requests, scrapy, shutil, time
+import os, requests, scrapy, shutil, time
 from urllib.parse import urljoin, urlparse
 
 debug = False
@@ -15,6 +15,7 @@ def get_pdf_link_scrapy(url):
         selector = scrapy.Selector(text=response.text)
 
         if debug: print("Procurando Links")
+
         for link in selector.xpath("//a"):
             link_xpath = link.xpath("@href").get()
 
@@ -49,14 +50,15 @@ def download_resource(url, save_folder="."):
         if os.path.isdir(filename):
             filename = os.path.join(filename, "downloaded_file")
 
-        with open(filename, 'wb') as file:
-            #file.write(response.content)
+        with open(filename, 'wb') as file:            
             start_time = time.time()
             if debug: print("Baixando arquivo")
             file.write(response.content)
 
-        if debug: print(f"Download concluido: {url}")
-        if debug: print(f"Tempo de download: {(time.time() - start_time):.2f} segundos")
+        if debug: 
+            print(f"Download concluido: {url}")
+            print(f"Tempo de download: {(time.time() - start_time):.2f} segundos")
+
     else:
         if debug: print(f"Erro ao fazer download: {url}")
 
@@ -67,6 +69,5 @@ def compact_downloaded_files():
 
 
 if __name__ == "__main__":
-    debug = True
     get_pdf_link_scrapy("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos")
     compact_downloaded_files()
